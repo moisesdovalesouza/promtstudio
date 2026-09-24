fc54c2df8a499a3509c939c9f9657c981be9d908
moisesdovalesouza <moisesdovalesouza@gmail.com>
2026-09-24 17:20:35 -0300

    PromtStudio 4.2.0 Beta

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE = 'promptstudio-v4.2.0';
+const CORE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/promptstudio-logo.svg',
+  './icons/icon-192.png',
+  './icons/apple-touch-icon.png',
+  './icons/logo-header-dark.png',
+  './icons/logo-header-light.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png',
+  './icons/favicon-32.png',
+  './icons/favicon-48.png',
+  './icons/favicon-16.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+      return response;
+    }).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      if (response && response.status === 200 && response.type === 'basic') {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }))
+  );
+});
